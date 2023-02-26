b1d5d8b89b2e15daebc475609f835ad361c104fe
172698691 <172698691@qq.com>
2023-02-26 17:08:40 +0800

    Impliment training RF with P_good and P_bad

--- surrogate.py
+++ surrogate.py
@@ -4,6 +4,8 @@ import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.core.survival import Survival
 
+from utils import get_non_dominated_solutions
+
 
 # define the algorithm
 class SurrogateNSGA2(NSGA2):
@@ -14,25 +16,42 @@ class SurrogateNSGA2(NSGA2):
 
     def _initialize(self):
         super()._initialize()
-        self.survival = MySurvival(self.survival, self.classifier_name)
+        self.survival = SurrogateSurvival(self.survival, self.classifier_name)
 
-class MySurvival(Survival):
+class SurrogateSurvival(Survival):
 
     def __init__(self, survival, classifier_name=None):
         super().__init__()
         self.survival = survival
         self.classifier_name = classifier_name
+        self.P_good = []
+        self.P_bad = []
 
     def _do(self, problem, pop, _parents=None, **kwargs):
         if self.classifier_name is None:
             return self.survival._do(problem, pop, _parents, **kwargs)
         elif self.classifier_name == "RandomForest":
             self.classifier = RandomForestClassifier()
+        
+        # update P_good and P_bad
+        Q = np.array(pop)
+        F = pop.get("F")
+        n = len(Q)
+        _, idx = get_non_dominated_solutions(F, return_index=True)
+        Q_good = Q[idx]
+        Q_bad = np.array([ind for ind in Q if ind not in Q_good])
+        all_good = np.concatenate([self.P_good, Q_good], axis=0)
+        _, idx = get_non_dominated_solutions([ind.F for ind in all_good], return_index=True)
+        all_good = all_good[idx]
+        all_bad = np.concatenate([self.P_bad, self.P_good, Q], axis=0)
+        all_bad = np.array([ind for ind in all_bad if ind not in all_good])
+        self.P_good = np.random.choice(all_good, size=min(5*n, len(all_good)), replace=False)
+        self.P_bad = np.random.choice(all_bad, size=min(5*n, len(all_bad)), replace=False)
 
         # predict the labels of offspring
         X = pop.get("X")
-        train_x = np.random.randint(low=0, high=2, size=(100, X.shape[-1]), dtype=bool)
-        train_y = np.random.choice([-1, 1], size=(100,))
+        train_x = np.concatenate([[ind.X for ind in self.P_good], [ind.X for ind in self.P_bad]], axis=0)
+        train_y = np.concatenate([np.ones(len(self.P_good)), -1*np.ones(len(self.P_bad))], axis=0)
         self.classifier.fit(train_x, train_y)
         y_pred = self.classifier.predict(X)
 
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def get_non_dominated_solutions(solutions):
+def get_non_dominated_solutions(solutions, return_index=False):
     def is_dominated(x, y):
         return all(x[i] <= y[i] for i in range(len(x))) and any(x[i] < y[i] for i in range(len(x)))
 
     non_dominated = []
+    non_dominated_indices = []
     for i, x in enumerate(solutions):
         dominated = False
         for j, y in enumerate(solutions):
@@ -14,4 +15,8 @@ def get_non_dominated_solutions(solutions):
                 break
         if not dominated:
             non_dominated.append(x)
-    return np.array(non_dominated)
+            non_dominated_indices.append(i)
+    if return_index:
+        return np.array(non_dominated), np.array(non_dominated_indices)
+    else:
+        return np.array(non_dominated)
