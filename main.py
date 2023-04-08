09398fd1fdd6a9612a366ebe269986782944805c
172698691 <172698691@qq.com>
2023-04-08 11:46:22 +0800

    Fix func eval error

--- main.py
+++ main.py
@@ -43,13 +43,14 @@ def main():
         capacity = 0.6*np.sum(weights)
         problem = Knapsack(values, volume, weights, capacity)
         pop_size = 50
+        n_offsprings=100
         max_eval = 20
 
         # define the surrogate model
         # classifier_name = "GB"
         # classifier_arg={'n_estimators': 100, 'learning_rate': 0.15, 'max_depth': 5}
         classifier_name = "RF"
-        classifier_arg={'n_estimators': 400, 'max_depth': 15, 'min_samples_split':3}
+        classifier_arg={'n_estimators': 300, 'max_depth': 12, 'min_samples_split': 3}
         # classifier_name = "SVM"
         # classifier_arg={'kernel': 'rbf', 'C': 1, 'gamma': 0.1}
         # classifier_name = "KNN"
@@ -59,22 +60,23 @@ def main():
 
         # define the algorithm
         ref_dirs = get_reference_directions("das-dennis", problem.n_obj, n_partitions=12)
-        algorithm = NSGA3(
+        algorithm = NSGA2(
             pop_size=pop_size,
+            n_offsprings=n_offsprings,
             sampling=BinaryRandomSampling(),
             crossover=TwoPointCrossover(),
             mutation=BitflipMutation(),
-            ref_dirs=ref_dirs,
+            # ref_dirs=ref_dirs,
             eliminate_duplicates=True
         )
-        surrogate_algorithm = SurrogateNSGA3(
+        surrogate_algorithm = SurrogateNSGA2(
             pop_size=pop_size,
+            n_offsprings=n_offsprings,
             sampling=BinaryRandomSampling(),
             crossover=TwoPointCrossover(),
             mutation=BitflipMutation(),
             eliminate_duplicates=True,
-            ref_dirs=ref_dirs,
-            arc=True,
+            # ref_dirs=ref_dirs,
             classifier_name=classifier_name,
             classifier_arg=classifier_arg,
             max_eval=max_eval
@@ -172,8 +174,9 @@ def main():
     plt.plot(surrogate_n_evals, surrogate_y_all, color='orange', label='Surrogate')
     plt.axhline(1, color="r", linestyle="--")
     plt.legend()
-    plt.title(f'{classifier_arg}')
-    plt.ylim(0.4, 1.05)
+    # plt.title(f'{classifier_arg}')
+    plt.title('NSGA-2')
+    plt.ylim(0.3, 1.05)
     plt.xlabel('Function evaluations', fontsize=25)
     plt.ylabel('HV', fontsize=25)
     plt.savefig('result.png')
--- surrogate.py
+++ surrogate.py
@@ -14,16 +14,14 @@ from utils import get_non_dominated_solutions
 
 class SurrogateSurvival(Survival):
 
-    def __init__(self, survival, arc=False, classifier_name=None, classifier_arg=dict(), max_eval=5, has_opt=False):
+    def __init__(self, survival, classifier_name=None, classifier_arg=dict(), max_eval=5, has_opt=False):
         super().__init__()
         self.survival = survival
-        self.arc = arc
         self.classifier_name = classifier_name
         self.classifier_arg = classifier_arg
         self.max_eval = max_eval
         self.has_opt = has_opt
-        self.P_good = []
-        self.P_bad = []
+        self.archive = []
 
     def _do(self, problem, pop, _parents=None, n_survive=None, **kwargs):
         if self.classifier_name is None:
@@ -41,44 +39,33 @@ class SurrogateSurvival(Survival):
         else:
             raise ValueError("Classifier name not recognized")
         
-        # update P_good and P_bad
-        Q = np.array(pop)
-        F = pop.get("F")
-        n = len(Q)
+        # update archive
+        for ind in kwargs['algorithm'].pop:
+            if ind not in self.archive:
+                self.archive.append(ind)
+        
+        # divide archive into good and bad
+        F = [ind.F for ind in self.archive]
         _, idx = get_non_dominated_solutions(F, return_index=True)
-        Q_good = Q[idx]
-        Q_bad = np.array([ind for ind in Q if ind not in Q_good])
-        all_good = np.concatenate([self.P_good, Q_good], axis=0)
-        _, idx = get_non_dominated_solutions([ind.F for ind in all_good], return_index=True)
-        all_good = all_good[idx]
-        all_bad = np.concatenate([self.P_bad, self.P_good, Q], axis=0)
-        all_bad = np.array([ind for ind in all_bad if ind not in all_good])
-        if self.arc:
-            arc_good = np.concatenate([self.P_good, all_good], axis=0)
-            _, idx = get_non_dominated_solutions([ind.F for ind in arc_good], return_index=True)
-            self.P_good = arc_good[idx]
-            arc_all = np.concatenate([self.P_bad, all_bad, arc_good], axis=0)
-            self.P_bad = np.array([ind for ind in arc_all if ind not in self.P_good])
-        else:
-            self.P_good = np.random.choice(all_good, size=min(5*n, len(all_good)), replace=False)
-            self.P_bad = np.random.choice(all_bad, size=min(5*n, len(all_bad)), replace=False)
+        A_good = np.array(self.archive)[idx]
+        A_bad = np.array([ind for ind in self.archive if ind not in A_good])
+
+        # train classifier
+        train_x = np.concatenate([[ind.X for ind in A_good], [ind.X for ind in A_bad]], axis=0)
+        train_y = np.concatenate([np.ones(len(A_good)), -1*np.ones(len(A_bad))], axis=0)
+        self.classifier.fit(train_x, train_y)
 
         # predict the labels of offspring
         X = pop.get("X")
-        train_x = np.concatenate([[ind.X for ind in self.P_good], [ind.X for ind in self.P_bad]], axis=0)
-        train_y = np.concatenate([np.ones(len(self.P_good)), -1*np.ones(len(self.P_bad))], axis=0)
-        self.classifier.fit(train_x, train_y)
         y_pred = self.classifier.predict(X)
 
         # select only the good offspring
         good_offspring = pop[y_pred == 1]
-
-        # return the result of the original survival if there is no good offspring
         if len(good_offspring) == 0:
             good_offspring = pop
-        
         good_offspring = good_offspring[np.random.choice(len(good_offspring), size=min(len(good_offspring), self.max_eval), replace=False)]
 
+        # do the original survival
         if self.has_opt:
             self.survival._do(problem=problem, pop=good_offspring, _parents=_parents, n_survive=n_survive, **kwargs)
             self.opt = self.survival.opt
@@ -92,41 +79,43 @@ class SurrogateSurvival(Survival):
 
 class SurrogateNSGA2(NSGA2):
 
-    def __init__(self, arc=False, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
+    def __init__(self, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
         super().__init__(**kwargs)
-        self.arc = arc
         self.classifier_name = classifier_name
         self.classifier_arg = classifier_arg
         self.max_eval = max_eval
 
     def _initialize(self):
         super()._initialize()
-        self.survival = SurrogateSurvival(self.survival, self.arc, self.classifier_name, self.classifier_arg, self.max_eval)
+        self.survival = SurrogateSurvival(self.survival, self.classifier_name, self.classifier_arg, self.max_eval)
+
+    def advance(self, infills=None, **kwargs):
+        if self.evaluator.n_eval > self.pop_size:
+            self.evaluator.n_eval = self.evaluator.n_eval - self.n_offsprings + self.max_eval
+        return super().advance(infills, **kwargs)
 
 
 class SurrogateNSGA3(NSGA3):
 
-    def __init__(self, arc=False, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
+    def __init__(self, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
         super().__init__(**kwargs)
-        self.arc = arc
         self.classifier_name = classifier_name
         self.classifier_arg = classifier_arg
         self.max_eval = max_eval
 
     def _initialize(self):
         super()._initialize()
-        self.survival = SurrogateSurvival(self.survival, self.arc, self.classifier_name, self.classifier_arg, self.max_eval, has_opt=True)
+        self.survival = SurrogateSurvival(self.survival, self.classifier_name, self.classifier_arg, self.max_eval, has_opt=True)
 
 
 class SurrogateRVEA(RVEA):
 
-    def __init__(self, arc=False, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
+    def __init__(self, classifier_name=None, classifier_arg=dict(), max_eval=5, **kwargs):
         super().__init__(**kwargs)
-        self.arc = arc
         self.classifier_name = classifier_name
         self.classifier_arg = classifier_arg
         self.max_eval = max_eval
 
     def _initialize(self):
         super()._initialize()
-        self.survival = SurrogateSurvival(self.survival, self.arc, self.classifier_name, self.classifier_arg, self.max_eval)
+        self.survival = SurrogateSurvival(self.survival, self.classifier_name, self.classifier_arg, self.max_eval)
